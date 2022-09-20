c6755b1e503ef0463c9334448b0385932edd512e
Nyan <85216339+ASPNyan@users.noreply.github.com>
2022-09-20 14:29:36 +0930

    Prepare for v1.1.0 release

---
+++ build/CJS/CommandJSONExport.cjs
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports["CommandJSONExport"] = class CommandJSONExport {
+    name;
+    description;
+    options;
+    default_member_permissions;
+    dm_permission;
+    name_localizations;
+    description_localizations;
+};
---
+++ build/CJS/CommandJSONExport.d.cts
@@ -0,0 +1,9 @@
+export class CommandJSONExport {
+    name: any;
+    description: any;
+    options: any;
+    default_member_permissions: any;
+    dm_permission: any;
+    name_localizations: any;
+    description_localizations: any;
+}
