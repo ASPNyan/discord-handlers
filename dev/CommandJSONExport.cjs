63d74741bdd37a595de97e1bc8542e28d585549b
Nyan <85216339+ASPNyan@users.noreply.github.com>
2022-09-20 14:21:13 +0930

    Reformat Dev Branch
    * Delete .gitignore
    
    * Delete dist directory
    
    * Delete LICENSE.md
    
    * Delete README.md
    
    * Delete package-lock.json
    
    * Delete package.json
    
    * Upload Changes

---
+++ dev/CommandJSONExport.cjs
@@ -0,0 +1,9 @@
+exports["CommandJSONExport"] = class CommandJSONExport {
+    name
+    description
+    options
+    default_member_permissions
+    dm_permission
+    name_localizations
+    description_localizations
+}
